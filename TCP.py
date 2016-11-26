e1144c45928770a1832b0fdc536d04eb6f345f0a
xiaotao <guxt1994@gmail.com>
2016-11-26 13:52:54 +0800

    fix

--- TCP.py
+++ TCP.py
@@ -14,34 +14,34 @@ MSGS = Queue.Queue(maxsize=1000)
 
 
 def connect(port, istest):
-    global FP
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    host = 'production'
-    if istest:
-        host = "test-exch-{}".format(Constant.TEAMNAME)
-    s.connect((host, port))
-    FP = s.makefile('w+', 1)
+	global FP
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	host = 'production'
+	if istest:
+		host = "test-exch-{}".format(Constant.TEAMNAME)
+	s.connect((host, port))
+	FP = s.makefile('w+', 1)
 
 
 def tread():
-    global FP
-    return FP.readline().strip()
+	global FP
+	return FP.readline().strip()
 
 def readall():
 	lines = FP.readlines()
 	return lines
 
 def twrite(contentDict):
-    global FP
-    cmd = json.dumps(contentDict)
+	global FP
+	cmd = json.dumps(contentDict)
 	return tread()
 
 def upload(contentDict):
-    respHello = twrite(Constant.MSG_HELLO)
-    if contentDict is None:
-        return respHello
-    resp = twrite(contentDict)
-    return resp
+	respHello = twrite(Constant.MSG_HELLO)
+	if contentDict is None:
+		return respHello
+	resp = twrite(contentDict)
+	return resp
 
 
 def hello():
@@ -69,13 +69,13 @@ def start(port, istrue):
 	thread.start_new_thread ( update_msg, () )
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='data process')
-    # parser.add_argument('--port', '-p', action="store_true", help='fast load data')
-    # parser.add_argument('--load', action="store_true", help='reload data, else load from cache')
-    parser.add_argument('--port', '-p', type=int, help='port', default=25000)
-    args = parser.parse_args()
+	parser = argparse.ArgumentParser(description='data process')
+	# parser.add_argument('--port', '-p', action="store_true", help='fast load data')
+	# parser.add_argument('--load', action="store_true", help='reload data, else load from cache')
+	parser.add_argument('--port', '-p', type=int, help='port', default=25000)
+	args = parser.parse_args()
 
-    connect(args.port)
-    resp = upload({"type": "add", "order_id": 1, "symbol": "BOND", "dir": "BUY", "price": 950, "size": 5})
-    print(resp)
+	connect(args.port)
+	resp = upload({"type": "add", "order_id": 1, "symbol": "BOND", "dir": "BUY", "price": 950, "size": 5})
+	print(resp)
 	
