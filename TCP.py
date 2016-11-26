09196dac5174f06a9815970a4c5b6e35a7603690
nbajiaoshi <nbajiaoshi@gmail.com>
2016-11-26 12:07:35 +0800

    reformat

--- TCP.py
+++ TCP.py
@@ -9,51 +9,58 @@ import Constant
 
 FP = None
 
+
 def connect(port, istest):
-	global FP
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	host = 'production'
-	if istest:
-		host = "test-exch-{}".format(Constant.TEAMNAME)
-	s.connect((host, port))
-	FP = s.makefile('w+', 1)
-	
+    global FP
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = 'production'
+    if istest:
+        host = "test-exch-{}".format(Constant.TEAMNAME)
+    s.connect((host, port))
+    FP = s.makefile('w+', 1)
+
+
 def tread():
-	global FP
-	return FP.readline().strip()
-	
+    global FP
+    return FP.readline().strip()
+
+
 def readall():
-	lines = []
-	line = tread()
-	while line:
-		lines.append(line)
-		line = tread()
-	return lines
-	
+    lines = []
+    line = tread()
+    while line:
+        lines.append(line)
+        line = tread()
+    return lines
+
+
 def twrite(contentDict):
-	global FP
-	cmd = json.dumps(contentDict)
-	print(cmd, file=FP)
-	return json.loads(tread())
+    global FP
+    cmd = json.dumps(contentDict)
+    print(cmd, file=FP)
+    return json.loads(tread())
+
 
 def upload(contentDict):
-	respHello = twrite(Constant.MSG_HELLO)
-	if contentDict is None:
-		return respHello
-	resp = twrite(contentDict)
-	return resp
-	
+    respHello = twrite(Constant.MSG_HELLO)
+    if contentDict is None:
+        return respHello
+    resp = twrite(contentDict)
+    return resp
+
+
 def hello():
-	respHello = twrite(Constant.MSG_HELLO)
-	return respHello
+    respHello = twrite(Constant.MSG_HELLO)
+    return respHello
+
 
 if __name__ == "__main__":
-	parser = argparse.ArgumentParser(description='data process')
-	# parser.add_argument('--port', '-p', action="store_true", help='fast load data')
-	# parser.add_argument('--load', action="store_true", help='reload data, else load from cache')
-	parser.add_argument('--port', '-p', type=int, help='port', default=25000)
-	args = parser.parse_args()
-	
-	connect(args.port)
-	resp = upload({"type": "add", "order_id": 1, "symbol": "BOND", "dir": "BUY", "price": 950, "size": 5})
-	print(resp)
+    parser = argparse.ArgumentParser(description='data process')
+    # parser.add_argument('--port', '-p', action="store_true", help='fast load data')
+    # parser.add_argument('--load', action="store_true", help='reload data, else load from cache')
+    parser.add_argument('--port', '-p', type=int, help='port', default=25000)
+    args = parser.parse_args()
+
+    connect(args.port)
+    resp = upload({"type": "add", "order_id": 1, "symbol": "BOND", "dir": "BUY", "price": 950, "size": 5})
+    print(resp)
