cf94fb1a1cb3ffa52420e40d9dc239711e7976ab
xiaotao <guxt1994@gmail.com>
2016-11-26 15:38:42 +0800

    f

--- Movement.py
+++ Movement.py
@@ -49,7 +49,9 @@ if __name__ == '__main__':
 		connect(args.port, args.istest)
 		print('connect ok')
 		hello()
+		print(zihao.HELLO_DICT)
 		print(zihao.BOOK_DICT)
+		time.sleep(5)
 	# while True:
 	# 	try:
 	# 		cmd = raw_input('CMD: b|s order_id symbol price size\n')
